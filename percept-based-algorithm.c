6d50f723475ed3807df55ead79203b275fd0b516
Amy <amy_ramnauth@yahoo.com>
2017-09-28 01:00:47 -0400

    Percept-based algorithm added

--- percept-based-algorithm.c
+++ percept-based-algorithm.c
@@ -1,4 +1,13 @@
+/*
+Date: 28-09-2017
+Author(s): 
+Tic-Tac-Toe Percept-Based Algorithm 
+Computer plays by selecting a random move at all times.
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define ROWS 3
 #define COLS 3
@@ -32,6 +41,7 @@ int main() {
 	int column = 0;
 	int line = 0;
 	int i;
+	int randChoice;
 
 	Board board = {
 		{'1','2','3'},
@@ -39,19 +49,27 @@ int main() {
 		{'7','8','9'}
 	};
 
-
+	/*To prevent sequence repetition between runs*/
+	srand(time(NULL)); 
+	
 	for (i = 0; i<9 && !winner; i++) {
 		int player = i%2 + 1;
 		printBoard(board);
 
 		do {
-			printf("\nPlayer %d, please enter the number of the square "
-				"where you want to place your %c: ",
-				player,(player==1)?'X':'O');
-			scanf("%d", &choice);
-
-			row = --choice/3;
-			column = choice%3;
+			
+			if (player==1) {
+				randChoice = rand() % 10;
+				row = --randChoice/3;
+				column = randChoice%3;
+			} else {
+				printf("\nPlease enter the number of the square "
+					"where you want to place your O: ");
+				scanf("%d", &choice);
+	
+				row = --choice/3;
+				column = choice%3;
+			}
 		} while(choice<0 || choice>9 || board [row][column]>'9');
 
 		board[row][column] = (player == 1) ? 'X' : 'O';
@@ -59,14 +77,18 @@ int main() {
 		if (hasWinner(board, line)) { winner = player; }
 	}
 	
+	
+	
 	printBoard(board);
 
 	if(!winner) {
 		printf("The game is a draw\n");
-	} else {
-		printf("Player %d has won\n", winner);
-	}
-
+	} 	else  if (winner==1){
+			printf("Computer has won\n");
+		}
+		else {
+			printf("You won\n");
+		}
 	return 0;
 }
 
