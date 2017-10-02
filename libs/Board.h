388726a5c849b368be7c192c8f0e047bc9647354
Phillip Gajadhar <phillipgajadhar@outlook.com>
2017-10-02 07:59:11 -0400

    Update return type of hasWinner to boolean

--- libs/Board.h
+++ libs/Board.h
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #define ROWS 3
 #define COLS 3
 typedef char Board[ROWS][COLS];
@@ -11,15 +12,15 @@ void printBoard(Board board) {
 	printf(" %c | %c | %c\n", board[2][0], board[2][1], board[2][2]);
 }
 
-int hasWinner(Board board) {
+bool hasWinner(Board board) {
 	int line;
 	if((board[0][0]==board[1][1] && board[0][0]==board[2][2]) ||
-		(board[0][2]==board[1][1] && board[0][2]==board[2][0])) { return 1; }
+		(board[0][2]==board[1][1] && board[0][2]==board[2][0])) { return true; }
 	for(line = 0; line <=2; line++) {
 		if((board[line][0]==board[line][1] && board[line][0]==board[line][2])||
 			(board[0][line]==board[1][line] && board[0][line]==board[2][line])) {
-				return 1;
+				return true;
 			}
 	}
-	return 0;
+	return false;
 }
